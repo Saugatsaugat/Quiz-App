f289300d1eb46800b9cdfa4909b7ea2308f2ee7c
Saugat Thapa <saugatthapa2073@gmail.com>
2025-06-19 12:27:31 -0500

    Modified REST APIs of Question model using ResponseEntity and exception Handling

--- src/main/java/com/saugat/quizapp/controller/QuestionController.java
+++ src/main/java/com/saugat/quizapp/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.saugat.quizapp.controller;
 import com.saugat.quizapp.model.Question;
 import com.saugat.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,32 +29,32 @@ public class QuestionController {
     }
 
     @GetMapping("getAll")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return service.getAllQuestions();
     }
 
     @GetMapping("question/{questionId}")
-    public Question get(@PathVariable("questionId") int questionId){
+    public ResponseEntity<Question> get(@PathVariable("questionId") int questionId){
         return service.getQuestionById(questionId);
     }
 
     @GetMapping("category/{category}")
-    public List<Question> findQuestionsByCategory(@PathVariable("category") String category){
+    public ResponseEntity<List<Question>> findQuestionsByCategory(@PathVariable("category") String category){
         return service.findQuestionsByCategory(category);
     }
 
     @PostMapping("question")
-    public Question add(@RequestBody Question question){
+    public ResponseEntity<Question> add(@RequestBody Question question){
         return service.addorUpdateQuestion(question);
     }
 
     @PutMapping("question")
-    public Question update(@RequestBody Question question){
+    public ResponseEntity<Question> update(@RequestBody Question question){
         return service.addorUpdateQuestion(question);
     }
 
     @DeleteMapping("question/{questionId}")
-    public String delete(@PathVariable("questionId") int questionId){
+    public ResponseEntity<String> delete(@PathVariable("questionId") int questionId){
         return service.deleteQuestion(questionId);
     }
 
--- src/main/java/com/saugat/quizapp/service/QuestionService.java
+++ src/main/java/com/saugat/quizapp/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.saugat.quizapp.service;
 import com.saugat.quizapp.model.Question;
 import com.saugat.quizapp.repo.QuestionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,25 +24,52 @@ public class QuestionService {
         this.repo = repo;
     }
 
-    public List<Question> getAllQuestions(){
-        return repo.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        try {
+            return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> findQuestionsByCategory(String category){
-        return repo.findByCategory(category);
+    public ResponseEntity<List<Question>> findQuestionsByCategory(String category){
+        try {
+            return new ResponseEntity<>(repo.findByCategory(category), HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NOT_FOUND);
     }
 
 
-    public Question addorUpdateQuestion(Question question) {
-        return repo.save(question);
+    public ResponseEntity<Question> addorUpdateQuestion(Question question) {
+
+        try {
+            return new ResponseEntity<>(repo.save(question), HttpStatus.CREATED);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
     }
 
-    public String deleteQuestion(int questionId) {
-        repo.deleteById(questionId);
-        return "Deleted";
+    public ResponseEntity<String> deleteQuestion(int questionId) {
+        try {
+            repo.deleteById(questionId);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+
     }
 
-    public Question getQuestionById(int questionId) {
-        return repo.findById(questionId).get();
+    public ResponseEntity<Question> getQuestionById(int questionId) {
+        try {
+            return new ResponseEntity<>(repo.findById(questionId).get(), HttpStatus.CREATED);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 }
